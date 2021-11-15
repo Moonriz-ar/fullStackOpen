46477d765da25124b966c83d82eaa8f40a75a3dd
Andrea Lin <andre93.lin@gmail.com>
2021-11-15 09:53:06 -0300

    finished exercise 1.7

--- part1/unicafe/src/components/Statistics.js
+++ part1/unicafe/src/components/Statistics.js
@@ -7,6 +7,8 @@ function Statistics({ good, neutral, bad }) {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>positive {(good / (good + neutral + bad)) * 100} %</p>
     </>
   );
 }
