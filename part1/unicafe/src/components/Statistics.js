7c9922b3e765c47e7f38d9a67066ec96ca4015cd
Andrea Lin <andre93.lin@gmail.com>
2021-11-15 10:06:06 -0300

    finished exercise 1.9 conditional rendering

--- part1/unicafe/src/components/Statistics.js
+++ part1/unicafe/src/components/Statistics.js
@@ -1,14 +1,28 @@
 import React from "react";
 
 function Statistics({ good, neutral, bad }) {
+  let sum = good + neutral + bad;
+
+  let statistics;
+  // conditional rendering
+  if (sum === 0) {
+    statistics = <p>No Feedback given</p>;
+  } else {
+    statistics = (
+      <>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {sum}</p>
+        <p>positive {(good / sum) * 100} %</p>
+      </>
+    );
+  }
+
   return (
     <>
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>positive {(good / (good + neutral + bad)) * 100} %</p>
+      {statistics}
     </>
   );
 }
