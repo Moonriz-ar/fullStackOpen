08ad3f116c2ea7f946a1095a4ece37066670f149
Andrea Lin <andre93.lin@gmail.com>
2021-12-22 17:11:46 -0300

    completed exercise 6.9 anecdotes - refactor and move redux store into its own file

---
+++ part6/redux-anecdotes/src/app/store.js
@@ -0,0 +1,7 @@
+import { createStore, combineReducers } from "redux";
+import reducer from "../reducers/anecdoteReducer";
+
+export const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
